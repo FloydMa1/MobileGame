512b8f4ca725a64634aa8ddd2aa346015e7ca8ae
Joppe van Basten <21983@ma-web.nl>
2017-11-21 14:48:57 +0100

    scenes

---
+++ Assets/Joppe/Scripts/SceneManagerr.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerr : MonoBehaviour {
+
+     public void StartGame()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("StartScherm", LoadSceneMode.Single);
+    }
+}
