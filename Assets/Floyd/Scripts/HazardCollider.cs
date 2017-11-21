b55be65c99795c16f53112f474608779353a4680
FloydMa1 <22355@ma-web.nl>
2017-11-21 11:53:15 +0100

    FirstCommit

---
+++ Assets/Floyd/Scripts/HazardCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardCollider : MonoBehaviour {
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Platform")
+        {
+            this.gameObject.SetActive(false);
+        }
+
+        if(collision.gameObject.tag == "player")
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Floyd/Scripts/SpawnBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBehavior : MonoBehaviour
+{
+
+
+    public float minX = 0.0f;
+    public float maxX = 0.0f;
+    public GameObject[] Hazards;
+    public float timeBetweenSpawn = 0.0f;
+    private bool canSpawn = false;
+    private int amountOfHazardsToSpawn = 0;
+    private int hazardToSpawn = 0;
+
+	void Start ()
+    {
+        canSpawn = true;
+	}
+	
+	void Update ()
+    {
+        if (canSpawn == true)
+        {
+            StartCoroutine(GenerateHazard());
+        }
+	}
+
+    private IEnumerator GenerateHazard()
+    {
+        canSpawn = false;
+        timeBetweenSpawn = Random.Range(0.5f, 2.0f);
+        amountOfHazardsToSpawn = Random.Range(1, 6);
+        for (int i = 0; i < amountOfHazardsToSpawn; i++)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(minX, maxX), 8.0f, 0.0f);
+            Instantiate(Hazards[hazardToSpawn], spawnPos, Quaternion.identity);
+        }
+        yield return new WaitForSeconds(timeBetweenSpawn);
+        canSpawn = true;
+    }
+}
